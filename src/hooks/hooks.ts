d348a380689c08c7990c40041f2f3edf83595748
ADennis450 <ADennis450@gmail.com>
2024-12-01 16:04:27 -0500

    updated hooks name

--- src/hooks/hooks.ts
+++ src/hooks/hooks.ts
@@ -8,45 +8,42 @@ let browser: Browser;
 let context: BrowserContext;
 
 BeforeAll(async () => {
-    getEnv();
-    browser = await getBrowser();
+  getEnv();
+  browser = await getBrowser();
 });
 
-Before( async function ({ pickle }) {
-    const scenarioName = pickle.name + pickle.id
-    context = await browser.newContext({
-        recordVideo: {
-            dir: "test-results/videos",
-        },
-    });
-    await context.tracing.start({
-        name: scenarioName,
-        title: pickle.name,
-        sources: true,
-        screenshots: true, snapshots: true
-    });
-    CurrentPage.page = await context.newPage();
+Before(async function ({ pickle }) {
+  const scenarioName = pickle.name + pickle.id;
+  context = await browser.newContext({
+    recordVideo: {
+      dir: "test-results/videos",
+    },
+  });
+  await context.tracing.start({
+    name: scenarioName,
+    title: pickle.name,
+    sources: true,
+    screenshots: true,
+    snapshots: true,
+  });
+  CurrentPage.page = await context.newPage();
 });
 
 After(async function ({ pickle, result }) {
-    let videoPath: string;
-    let img: Buffer;
-    const path = `./test-results/trace/${pickle.id}.zip`;
-    if (result?.status != Status.FAILED) {
-        img = await CurrentPage.page.screenshot(
-            { path: `./test-results/screenshots/${pickle.name}.png`, type: "png" })
-        await this.attach(
-            img, "image/png"
-        );
-    }
-    await CurrentPage.page.close();
-    await context.close();
+  let videoPath: string;
+  let img: Buffer;
+  const path = `./test-results/trace/${pickle.id}.zip`;
+  if (result?.status == Status.FAILED) {
+    img = await CurrentPage.page.screenshot({
+      path: `./test-results/screenshots/${pickle.name}.png`,
+      type: "png",
+    });
+    await this.attach(img, "image/png");
+  }
+  await CurrentPage.page.close();
+  await context.close();
 });
 
 AfterAll(async function () {
-    await browser.close();
-})
-
-
-
-
+  await browser.close();
+});
