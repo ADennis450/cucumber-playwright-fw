e3f1692e03328fe138d0218f0b1ece5f7689a71e
ADennis450 <ADennis450@gmail.com>
2025-01-19 19:42:26 -0500

    updated ApiUtil to use playwright api call

--- src/utils/ApiUtil.ts
+++ src/utils/ApiUtil.ts
@@ -1,14 +1,19 @@
+
+import { APIResponse } from "@playwright/test";
+import { CurrentPage } from "./CurrentPage";
+
 export class ApiUtil {
     private static apiResponse: Map<string, any>;
     public static async fetchData(url:string, method: 'GET'|'POST', body?: string,  headers?: any): Promise<string> {
-        const response = await fetch(url,
-           {
-            method: method,
-            body: body
-           }
-        )
-        console.log()
-        return JSON.stringify(await response.json())
+        let response: APIResponse;
+        if (method == 'GET') {
+            response = await CurrentPage.page.request.get(url)
+        } else if( method == 'POST') {
+            response = await CurrentPage.page.request.post(url,{data: body})
+        } else {
+            throw new Error('fetchData must be either a GET or POST Request')
+        }
+        return await response.text();
     };
     public static setApiResponse(key: string, value: string): void {
         if(this.apiResponse == null) {
